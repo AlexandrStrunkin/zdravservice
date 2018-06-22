629b41183ae3a5f95fb6378e42c91fef2672bd57
Ruslan <br@webgk.ru>
2018-06-22 18:35:59 +0300

    fix

--- ajax/regFormValidation.php
+++ ajax/regFormValidation.php
@@ -1,7 +1,7 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
-if ($_REQUEST["REGISTER[PERSONAL_PHONE]"] && $_REQUEST["REGISTER[EMAIL]"] && check_bitrix_sessid()) {
-    $phoneNumber = \Webgk\Main\Tools::updateUserPhoneOnRegForm($_REQUEST["REGISTER[PERSONAL_PHONE]"]);
-    $rsUsers = CUser::GetList(($by = "timestamp_x"), ($order = "desc"), array('LOGIC' => 'OR', array("PERSONAL_PHONE" => $phoneNumber), array("EMAIL" => $_REQUEST["REGISTER[EMAIL]"])) );
+if ($_REQUEST["REGISTER"]["PERSONAL_PHONE"] && $_REQUEST["REGISTER"]["EMAIL"] && check_bitrix_sessid()) {
+    $phoneNumber = \Webgk\Main\Tools::updateUserPhoneOnRegForm($_REQUEST["REGISTER"]["PERSONAL_PHONE"]);
+    $rsUsers = CUser::GetList(($by = "timestamp_x"), ($order = "desc"), array('LOGIC' => 'OR', array("PERSONAL_PHONE" => $phoneNumber), array("EMAIL" => $_REQUEST["REGISTER"]["EMAIL"])) );
     if ($rsUsers->SelectedRowsCount() <= 0) {
         $result = "ok";
     } else {
