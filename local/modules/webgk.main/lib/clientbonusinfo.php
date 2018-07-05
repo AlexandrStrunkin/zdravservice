392cc6f74c12bd42df8c96a5d16cd1d5c0f3cb45
Ruslan <br@webgk.ru>
2018-07-05 17:56:58 +0300

    fix

--- local/modules/webgk.main/lib/clientbonusinfo.php
+++ local/modules/webgk.main/lib/clientbonusinfo.php
@@ -56,8 +56,6 @@
                 $result["error"] .= "не удалось соединиться с вебсервисом! \n";
                 return $result;
             }        
-            print_r($clientInfo);
-            die();
             $clientInfoArr = array();
             // Закрываем запрос и удаляем инициализацию $curl
             if ($clientInfo) {
