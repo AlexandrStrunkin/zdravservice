c1bb047b57abf1544e0beb2c141dcba73ae51cf6
Alexander <faster@webgk.ru>
2018-07-06 11:00:34 +0300

    CP-5659: create prder template, create html for template

---
+++ local/components/webgk/sale.order/lang/ru/.parameters.php
@@ -0,0 +1,12 @@
+<?
+
+$MESS["COMP_PROP_DEFAULT_RATIO_LENGTH"] = "Длина трубы, в случае если в таблице коэффициентов будут отсутсвовать значения";
+$MESS["COMP_PROP_DEFAULT_RATIO_MIN_QUANTITY"] = "Минимальное количесво трубы, доступное для покупки";
+$MESS["COMP_PROP_AJAX_UPDATE_PRODUCT_URL"] = "url для ajax запроса на обновление товара";
+$MESS["COMP_PROP_AJAX_CLEAR_BASKET_URL"] = "url для ajax запроса на очистку корзины";
+$MESS["COMP_PROP_CATALOG_PAGE_URL"] = "url страницы каталога";
+$MESS["COMP_PROP_ORDER_PAGE_URL"] = "url страницы оформления товара";
+$MESS["COMP_PROP_AJAX_ADD_COMPARE_URL"] = "url для ajax запроса на добавление в сравнение";
+$MESS["COMP_PROP_AJAX_ADD_FAVORITE_URL"] = "url для ajax запроса на добавление в избранное";
+$MESS["COMP_PROP_LOYALTY_PAGE_URL"] = "url страницы программы лояльности";
+$MESS["COMP_PROP_QUESTION_PHONE"] = "номер телефона";
